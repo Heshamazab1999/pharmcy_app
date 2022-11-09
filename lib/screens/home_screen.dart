5922bd2273c6472690a834f471e1d555e5e9e050
Heshamazab <60970771+Heshamazab1999@users.noreply.github.com>
2022-11-09 11:34:05 -0800

    finish

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -23,147 +23,155 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
         floatingActionButton: FloatingActionButton(
           onPressed:
-              // If not yet listening for speech start, otherwise stop
-              controller.speechToText.value.isNotListening
-                  ? controller.startListening
-                  : controller.stopListening,
+          // If not yet listening for speech start, otherwise stop
+          controller.speechToText.value.isNotListening
+              ? controller.startListening
+              : controller.stopListening,
           tooltip: 'Listen',
-          child: Obx(() => Icon(controller.speechToText.value.isNotListening
-              ? Icons.mic_off
-              : Icons.mic)),
+          child: Obx(() =>
+              Icon(controller.speechToText.value.isNotListening
+                  ? Icons.mic_off
+                  : Icons.mic)),
         ),
         body: Obx(
-          () => controller.state == ViewState.busy
+              () =>
+          controller.state == ViewState.busy
               ? const ShimmerWidget()
               : SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: Dimensions.height * 0.1),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 20),
-                        child: Text("Medicine Store",
-                            style: TextStyle(
-                                color: ColorResources.Black_COLOR,
-                                fontSize: 22,
-                                fontWeight: FontWeight.bold)),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          // const Icon(
-                          //   Icons.shopping_cart_outlined,
-                          //   color: ColorResources.CARD_SHADOW_COLOR,
-                          // ),
-                          SizedBox(width: Dimensions.width * 0.05),
-                          Container(
-                              clipBehavior: Clip.antiAlias,
-                              height: 40,
-                              width: 40,
-                              decoration:
-                                  const BoxDecoration(shape: BoxShape.circle),
-                              child: Image.asset(
-                                Images.logo,
-                                fit: BoxFit.cover,
-                              )),
-                          SizedBox(width: Dimensions.width * 0.05),
-                        ],
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 20),
-                        child: Text("How are you felling today ? ",
-                            style: TextStyle(
-                                color: ColorResources.Disable_COL0R,
-                                fontSize: 18,
-                                fontWeight: FontWeight.w400)),
-                      ),
-                      SizedBox(height: Dimensions.height * 0.03),
-                      FixedTextField(
-                        label: "Search By any information",
-                        type: TextInputType.name,
-                        function: (v) {
-                          controller.onFilter(v);
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: Dimensions.height * 0.1),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Text("Medicine Store",
+                      style: TextStyle(
+                          color: ColorResources.Black_COLOR,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold)),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    // const Icon(
+                    //   Icons.shopping_cart_outlined,
+                    //   color: ColorResources.CARD_SHADOW_COLOR,
+                    // ),
+                    SizedBox(width: Dimensions.width * 0.05),
+                    Container(
+                        clipBehavior: Clip.antiAlias,
+                        height: 40,
+                        width: 40,
+                        decoration:
+                        const BoxDecoration(shape: BoxShape.circle),
+                        child: Image.asset(
+                          Images.logo,
+                          fit: BoxFit.cover,
+                        )),
+                    SizedBox(width: Dimensions.width * 0.05),
+                  ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Text("How are you felling today ? ",
+                      style: TextStyle(
+                          color: ColorResources.Disable_COL0R,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400)),
+                ),
+                SizedBox(height: Dimensions.height * 0.03),
+                FixedTextField(
+                  label: "Search By any information",
+                  type: TextInputType.name,
+                  function: (v) {
+                    controller.onFilter(v);
+                  },
+                ),
+                if ((controller.isSearched
+                    ? controller.categorySearch.length
+                    : controller.medicine.length) ==
+                    0)
+                  const EmptyPlaceholder(
+                    message: "Not Found Category",
+                  )
+                else
+                  SizedBox(
+                    height: MediaQuery
+                        .of(context)
+                        .size
+                        .height,
+                    width: MediaQuery
+                        .of(context)
+                        .size
+                        .width,
+                    child: SmartRefresher(
+                      controller: controller.refreshController,
+                      enablePullUp: true,
+                      enablePullDown: true,
+                      onLoading: () async {
+                        await controller.loadMore();
+                        // controller.refreshController.loadComplete();
+                        // controller.refreshController.requestLoading();
+                      },
+                      child: GridView.builder(
+                        // controller: controller.scrollController,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 15, vertical: 15),
+                        physics: const BouncingScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: controller.isSearched
+                            ? controller.categorySearch.length
+                            : controller.medicine.length,
+                        gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            childAspectRatio: 3 / 4,
+                            crossAxisSpacing: 20,
+                            mainAxisSpacing: 20),
+                        itemBuilder: (_, index) {
+                          return GestureDetector(
+                            onTap: () {
+                              Utility.showBottomSheet(
+                                  context,
+                                  GestureDetector(
+                                    onTap: () {
+                                      controller.launchUrlMedScape(
+                                          controller.isSearched
+                                              ? controller
+                                              .categorySearch[index].d
+                                              : controller
+                                              .medicine[index].d);
+                                    },
+                                    child: FixedBottomSheet(
+                                      onTap: () {
+                                        controller.launchUrlMethod(
+                                            controller.isSearched
+                                                ? controller
+                                                .categorySearch[index]
+                                                .d
+                                                : controller
+                                                .medicine[index].d);
+                                      },
+                                      medicine: controller.isSearched
+                                          ? controller
+                                          .categorySearch[index]
+                                          : controller.medicine[index],
+                                    ),
+                                  ));
+                            },
+                            child: CardScreen(
+                              medicine: controller.isSearched
+                                  ? controller.categorySearch[index]
+                                  : controller.medicine[index],
+                            ),
+                          );
                         },
                       ),
-                      if ((controller.isSearched
-                              ? controller.categorySearch.length
-                              : controller.medicine.length) ==
-                          0)
-                        const EmptyPlaceholder(
-                          message: "Not Found Category",
-                        )
-                      else
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height,
-                          width: MediaQuery.of(context).size.width,
-                          child: SmartRefresher(
-                            controller: controller.refreshController,
-                            enablePullUp: true,
-                            enablePullDown: true,
-                            onLoading: () async {
-                              await controller.loadMore();
-                              // controller.refreshController.loadComplete();
-                              // controller.refreshController.requestLoading();
-                            },
-                            child: GridView.builder(
-                              // controller: controller.scrollController,
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 15, vertical: 15),
-                              physics: const BouncingScrollPhysics(),
-                              shrinkWrap: true,
-                              itemCount: controller.isSearched
-                                  ? controller.categorySearch.length
-                                  : controller.medicine.length,
-                              gridDelegate:
-                                  const SliverGridDelegateWithFixedCrossAxisCount(
-                                      crossAxisCount: 2,
-                                      childAspectRatio: 3 / 4,
-                                      crossAxisSpacing: 20,
-                                      mainAxisSpacing: 20),
-                              itemBuilder: (_, index) {
-                                return GestureDetector(
-                                  onTap: () {
-                                    Utility.showBottomSheet(
-                                        context,
-                                        GestureDetector(
-                                          onTap: () {
-                                            controller.launchUrlMedScape(
-                                                controller.isSearched
-                                                    ? controller
-                                                        .categorySearch[index].d
-                                                    : controller
-                                                        .medicine[index].d);
-                                          },
-                                          child: FixedBottomSheet(
-                                            onTap: () {
-                                              controller.launchUrlMethod(
-                                                  controller.isSearched
-                                                      ? controller
-                                                          .categorySearch[index]
-                                                          .d
-                                                      : controller
-                                                          .medicine[index].d);
-                                            },
-                                            medicine: controller.isSearched
-                                                ? controller
-                                                    .categorySearch[index]
-                                                : controller.medicine[index],
-                                          ),
-                                        ));
-                                  },
-                                  child: CardScreen(
-                                    medicine: controller.isSearched
-                                        ? controller.categorySearch[index]
-                                        : controller.medicine[index],
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
-                        ),
-                    ],
+                    ),
                   ),
-                ),
+              ],
+            ),
+          ),
         ));
   }
 }
